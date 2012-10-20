caa9c0676373727dd71e2fd1e7bd9c548e384a3a
Andrew O'Neill <foolusion@gmail.com>
2012-10-20 11:37:41 -0400

    data structures for displaying pages

--- money.go
+++ money.go
@@ -5,15 +5,44 @@ import (
 	"net/http"
 )
 
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+type Invoice struct {
+	Amount  float64
+	Subject string
+	Details string
+	Tags    []string
+	Account
+}
+
+type Head struct {
+	Selected string
+	Options  []string
+}
+
+type PageContext struct {
+	Head
+	Invoices []Invoice
+	Tags     []string
+}
+
 func init() {
 	http.HandleFunc("/", homeHandler)
-	http.Handle("/stylesheets/", http.StripPrefix("/stylesheets/", http.FileServer(http.Dir("stylesheets"))))
+	http.Handle(
+		"/stylesheets/",
+		http.StripPrefix(
+			"/stylesheets/",
+			http.FileServer(http.Dir("stylesheets"))))
 }
 
 var homeTemplate = template.Must(template.ParseFiles("index.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	var p PageContext
+	if err := homeTemplate.Execute(w, p); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
